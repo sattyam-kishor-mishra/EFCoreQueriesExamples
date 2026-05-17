9506ec3b4ac3d06883776d5fbffe4e4b48faad02
SATTYAMMISHRA <mishra.sattyamkishor@gmail.com>
2026-05-17 20:33:01 +0530

    Enter new code connection

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using EFCoreQueriesExample.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreQueriesExample.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+
+
+        override protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Configure the relationship between Customer and Order
+            modelBuilder.Entity<Order>()
+                .HasOne<Customer>()
+                .WithMany()
+                .HasForeignKey(o => o.CustomerId);
+        }
+
+
+
+    }
+}
